e4ea23431bc1d51b47c86e9b07ee68d59a87572e
smcgowan@flexiant.com <smcgowan@flexiant.com>
2015-09-18 11:18:08 +0100

    EXTL-8835 - Update to twitter widget to fix parsing tweets
    update to all custom widgets to make get content call synchronous to
    avoid job spamming in manage pr page

--- plugins/youtube-custom-widget/youtube-custom-widget.lua
+++ plugins/youtube-custom-widget/youtube-custom-widget.lua
@@ -28,7 +28,7 @@ function youtube_widget_provider()
   table.insert(configurableList, videoIdParam);
 
   local actionFunctions = {};
-  table.insert(actionFunctions, widgetHelper:getContentActionDefinition("get_content_function", {"BE", "CUSTOMER"}));
+  table.insert(actionFunctions, widgetHelper:getContentActionDefinition("get_content_function", {"BE", "CUSTOMER"}, nil, true));
 
 
   return {
